7b67aa587f7d648198dbe2129e115975bb4baeb5
Mohamed KHIR <khir.mohamed5@gmail.com>
2023-08-30 16:10:24 +0200

    validation dans les models vu avec Jonathan

--- app/models/team.rb
+++ app/models/team.rb
@@ -4,4 +4,6 @@ class Team < ApplicationRecord
   has_many :team_games, class_name: 'Game', foreign_key: 'team_id'
   has_many :opponent_team_games, class_name: 'Game', foreign_key: 'opponent_team_id'
   has_many :players
+
+  validates :name, presence: true, uniqueness: true
 end
