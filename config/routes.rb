759867656b6c0f4bb994ed6d04a53fd084d6f976
joao cortat <jmcortat@gmail.com>
2023-09-01 10:02:31 +0200

    bug route

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  get "/dashboard", to: "pages#dashboard"
+  get "/dashboard", to: "dashboards#show", as: :dashboard
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-
-  get '/dashboard' => 'dashboards#show', as: :dashboard
-
   resources :teams do
     resources :players, only: %i[index new create edit update]
   end
