e8d216ca975146ab7c5a3ec4e8296ad326d7e25d
joao cortat <jmcortat@gmail.com>
2023-09-04 14:32:26 +0200

    parcial gameActions

--- app/models/player_action.rb
+++ app/models/player_action.rb
@@ -3,6 +3,6 @@ class PlayerAction < ApplicationRecord
   belongs_to :game
 
   KIND = %w[shot steal]
-  POSITION = ['back center', 'back left']
+  POSITION = ['L2M', 'C2M', 'R2M', 'Back left', 'Back center', 'Back right', 'F6M']
   RESULT = ['post', 'saved', 'goal', 'missed', 'blocked', 'corner']
 end
